5e362f88afc16823506cb9d3f839df2b19c7865c
Mark S <49697384+C64-Mark@users.noreply.github.com>
2020-03-01 19:21:59 +0000

    Delete Menu.asm

--- 10-Psion Attack/Menu.asm
+++
@@ -1,9 +0,0 @@
-Menu_Display
-        LIBSCREEN_SETCOLOURS_VV black, black
-        LIBSCREEN_SET1000_AV SCREENRAM, space
-        LIBSCREEN_COPYSCREEN_AA SCN_INTRO, COL_INTRO
-@waitfire
-        LIBJOY_GETJOY_V JoyFire
-        bne @waitfire
-        rts
-
